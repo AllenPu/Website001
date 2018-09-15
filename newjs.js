043ade170822933c0f6526b94c57efb8cc139529
AllenPu <18651885620@163.com>
2018-09-15 00:27:16 -0400

    Add files via upload

---
+++ newjs.js
@@ -0,0 +1,21 @@
+function rightornot() {
+    var x=document.getElementById("2333").value;
+    if (x == "Running"){
+        alert("wrong!")
+    }else if (x == "Hiking"){
+        alert("Right!")
+    }else if (x == "Ball Games"){
+        alert("Right")
+    }else if (x == "Video Games"){
+        alert("Wrong")
+    }
+
+}
+
+function right() {
+    alert("right!!!")
+}
+
+function wrong() {
+    alert("wrong!!!")
+}
